4a298987d24bf3ad55052a94d8e8680e6a73f54d
Syeda-Kashfa <kseyeda346@gmail.com>
2025-08-30 03:21:11 -0700

    complete chap 1

---
+++ Chap1/app.js
@@ -0,0 +1,21 @@
+// "Question 1"
+// alert ("Hello JavaScript");
+
+// "Question 2"
+// alert ("Error!please enter a valid password.");
+
+// "Question 3"
+// alert ("Welcome to JS Land...\nHappy Coding!");
+
+// "Question 4"
+// alert ("Welcome to JS Land...");
+
+
+// "Question 5-a"
+// alert("Happy Coding!\nprevent this pagefrom creating additional dialogs.");
+
+// "Question 5-b"
+// console.log("hello... ","I can run JS through my web browser's console");
+
+// Question no 6
+
