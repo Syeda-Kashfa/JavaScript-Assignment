241bf9a0d08652d90ac75af8a45f3573edbc5be7
Syeda-Kashfa <kseyeda346@gmail.com>
2025-09-04 03:44:54 -0700

    assignment done

---
+++ Assignment3/Chap3/app.js
@@ -0,0 +1,25 @@
+// Qusetion no # 1
+// let age = 20
+// alert("I am " + age + " years old");
+// let age1= "I am 20 years old"
+// alert(age1);
+// ________________________________________
+// Qusetion no # 2
+// let visiting = 14
+// alert("You have visited this site" + visiting + " times");
+// ________________________________________
+// Qusetion no # 3
+
+//   let birthYear ="My birth year is 2003"
+//    document.write(birthYear + "<br/>"  + "Data tyoe of my declared variable is number " );
+ 
+
+// ________________________________________
+// Qusetion no # 4
+//  let firstName ="John Doe"
+// let productTitle = "5 T-Shirts(s)"
+// let orderPlace = "XYZ clothing store"
+// let allSum = firstName + " ordered" + " " + productTitle + " on" + " " + orderPlace
+// document.write(allSum);
+
+
