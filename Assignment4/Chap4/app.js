119a3de293e89122e6e41ac9430c3621dd9be723
Syeda-Kashfa <kseyeda346@gmail.com>
2025-09-04 11:58:39 -0700

    Chapter 4 complete

---
+++ Assignment4/Chap4/app.js
@@ -0,0 +1,48 @@
+// Question No#1
+// let name ; let fatherName ; let fullName
+
+// Question No#2
+// Legal variables
+// 1: let number;
+// 2: let cnicNumber;
+// 3: let flower;
+// 4: let color;
+// 5: let model;
+
+// IllLegal Variables
+// 1: let var;
+// 2: let alert;
+// 3: let cosole.log;
+// 4: let document.write;
+// 5: let const;
+
+// Question No#3:a
+//document.write("<h1>Rules for naming JS variables</h1>")
+// Question No#3:b
+// 1.("A to Z letters")
+// 2.(lettersWithNumbers)
+// 3.(Under_Score)
+// 4.($ sign)
+
+// Question No#3:c
+
+// 1.("A to Z letters")
+// 2.(lettersWithNumbers)
+// 3.(Under_Score)
+// 4.($ sign)
+
+// Question No#3:d
+// Case_sensitive/Camel_case
+
+// Question No#3:e
+//Keywords some eaxple are given
+// let var, 
+// let if, 
+// let const, 
+// let else, 
+// let for, 
+// let function, 
+// let return;
+
+// document.write("<p> Variable name can only contain,Numbers,$,_, for exam:$my_1st variableple<br/>Variables must begin with letter $ or _,for example $my_1stVariable <br/> Variable names are case sensitive <br/> Variable names should not be JS keywords</p>")
+
