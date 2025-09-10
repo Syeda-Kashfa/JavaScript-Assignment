dbb877f5b2c933e544dd47ffc6306614ffe61ff6
Syeda-Kashfa <kseyeda346@gmail.com>
2025-09-10 08:38:50 -0700

    complete work

---
+++ Assignment5/Chap5/app.js
@@ -0,0 +1,275 @@
+// Question no#1
+
+// let num1 =3;
+// let num2 =5;
+// let newNum = num1 + num2;
+
+// console.log(`Sum of ${num1} and ${num2} is ${newNum}`);
+// console.log(`Sum of ${num1} and ${num2} is ${num1+num2}`);
+
+
+// Question no#2:a
+// Subtraction
+// let num1 =4;
+// let num2 =5;
+// let newNum = num1 - num2;
+
+// console.log(`Subtract of ${num1} and ${num2} is ${newNum}`);
+// console.log(`subtract of ${num1} and ${num2} is ${num2-num1}`);
+
+// Question no#2:b
+// Multiplication
+
+// let num1 =12;
+// let num2 =5;
+// let newNum = num1 * num2;
+
+// console.log(`Multiplication of ${num1} and ${num2} is ${newNum}`);
+// console.log(`Multiplication of ${num1} and ${num2} is ${num1-num2}`);
+
+// Question no#2:c
+// Division
+
+// let num1 =12;
+// let num2 =5;
+// let newNum = num1 / num2;
+
+// console.log(`Division of ${num1} and ${num2} is ${newNum}`);
+// console.log(`Division of ${num1} and ${num2} is ${num1/num2}`);
+
+// Question no#2:d
+// Modulus
+
+// let num1 =14;
+// let num2 =5;
+// let newNum = num1 % num2;
+
+// console.log(`Modulus oValue after variable declaration is : ${num}f ${num1} and ${num2} is ${newNum}`);
+// console.log(`Modulus of ${num1} and ${num2} is ${num1%num2}`);
+
+/*// Question no#3:a
+// let num 
+
+// Question no#3:b
+// let num = "??"
+// console.log(`Value after variable declaration is : ${num}`);
+
+
+// Question no#3:c
+// let num = 10
+// console.log(`Value after variable declaration is : ${num}`);
+
+// Question no#3:d
+// let num = 5
+// console.log(`Initial value is: ${num}`)
+
+// Question no#3:e
+ let num = 5
+num = ++num
+
+
+// Question no#3:f
+  console.log(`Value after increment  is: ${num}`);
+
+// Question no#3:g
+num = num + 7
+
+
+// Question no#3:h
+
+ console.log(`Value after addition is: ${num}`);
+
+// Question no#3:i
+ num = --num 
+
+
+// Question no#3:j
+ console.log(`Value after decrement is: ${num}`);
+
+// Question no#3:k
+let num1=3 
+
+// Question no#3:i
+let reminder = num % num1
+ console.log(`The reminder is: ${reminder}`)
+*/    
+// Summarize Question No # 3
+/* console.log(`Value after variable declaration is undefined
+Intial value is: 5
+Value after increment is: 6
+Value after addition is: 13
+Value after decrement is: 12
+The reminder is: 0`)*/
+
+// Question no#4
+// let movieTicket = 600
+// let buyTictkets = 5
+// console.log(`Total cost to buy ${buyTictkets} tickets to a movie is ${movieTicket*buyTictkets}PKR`)
+
+// Question no#4
+/*let table = 4
+let multiplyBy = 1
+multiplyBy = multiplyBy++
+
+console.log(`Table of 4`)
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);
+multiplyBy = ++multiplyBy
+console.log(`${table} * ${multiplyBy} = ${table*multiplyBy}`);*/
+
+// Question no#4
+// anotherway of table
+/*let numb = 4;
+console.log(`The table of 4 an other way`)
+for (let multiplyBy = 1; multiplyBy <= 10; multiplyBy++ ){
+    console.log(` ${numb} * ${multiplyBy} = ${numb * multiplyBy}`)
+}*/
+
+// Question no#6:a
+//  let celsius = 25
+
+// Question no#6:b
+  // let fehranheit = (celsius *9/5) + 32
+
+
+// Question no#6:c
+//  console.log(fehranheit)
+//   fehranheit = 77
+
+// Question no#6:d
+// let fehranheit1 =70
+// let celsius1 =(fehranheit1 -32) *5/9
+// console.log(celsius1)
+// document.write(`${celsius}C is ${fehranheit}<br/>
+// ${fehranheit1}F is ${celsius1}`)
+
+
+
+// Question no#7:a
+ // let item1 =650;
+
+// Question no#7:b
+ // let item2=100;
+
+// Question no#7:c
+ // let quant1=3;
+
+// Question no#7:d
+// let quant2=7
+
+// Question no#7:e
+/*let shippingCharges = 100;
+let totalCost =(item1 * quant1) + (item2 * quant2) + shippingCharges
+console.log(totalCost)
+document.write("<h1>Shopping Cart</h1>")
+document.write(`Price of item 1 is ${item1}<br/>
+Quantity of item 1 is ${quant1}<br/>
+Price of item 2 is ${item2}<br/>
+Quantity of item 2 is ${quant2} <br/>
+Shipping Charger ${shippingCharges}<br/>
+<br/>
+Total cost of your order is ${totalCost}`)*/
+
+// Question no#8
+/*document.write("<h1>Mark Sheet</h1>")
+let obtainedMarks = 804
+let totalMarks = 980
+document.write(`Total marks ${totalMarks} <br/>
+Marks obtained ${obtainedMarks}<br/>
+Percentage: ${obtainedMarks/totalMarks*100} `)*/
+
+// Question no#9
+/* let dollor =104.80
+let riyal =28
+let dollorQuantiy = 10
+let riyalQuantity = 25
+let totalCurrency = (dollor * dollorQuantiy) + (riyal * riyalQuantity)
+document.write(`<h1>Currency in PKR</h1> ${totalCurrency}`);
+console.log(`Total currency in PKR: ${totalCurrency}`)*/
+
+
+// Question no#10
+// let numb = 6
+
+// numb = (((numb + 5) * 10) /2)
+// document.write(`<h1>Arithmetic operatio</h1> ${numb}`)
+
+// // Question no#11:a
+// let currentYear = 2025
+// // Question no#11:b
+// let birthYear = 2003
+// // Question no#11:c
+// let age = currentYear - birthYear
+//  document.write(`<h1>Age Calculator</h1>
+//   Current Year: ${currentYear}<br/>
+//   Birthyear: ${birthYear}<br/>
+//   My age is: ${age}`)
+
+// Question no#12:a
+/* //let radius = 20
+// Question no#12:b
+let circumference = 2 * 3.142 * radius
+let area = 3.142 * radius * radius
+document.write(`<h1>The Geometrizer</h1><br/>
+Radius of a circle: ${radius}<br/>
+The circumference is: ${circumference}<br/>
+The area is: ${area}
+  `)*/
+
+  // Question no#12:a
+  // let fvrtSnack = "Pizzafries"
+
+  // // Question no#12:b
+  //   let age = 22
+
+  // // Question no#12:c
+  // let maximumAge = 40
+
+  
+  // // Question no#12:d
+  // let eatAmount = 1
+
+  //   // Question no#12:e
+  // let totalLifeEat = (40-22) * (365 * 1)
+
+  // document.write(`<h1>The Lifetime Supply Calculator</h1><br/>
+  // Favourite Snack:${fvrtSnack}<br/>
+  // Current age: ${age}<br/>
+  // Estimated maximum Age: ${maximumAge}<br/>
+  // Amount of snacks per day: ${eatAmount}<br/>
+  // You will need ${totalLifeEat} Pizza-fries to last you untill the ripe old age of ${maximumAge}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
