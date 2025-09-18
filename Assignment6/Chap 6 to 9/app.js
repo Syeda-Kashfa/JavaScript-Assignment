3b845463b38cb9382b1962dbb39fbdc70adf9152
Syeda-Kashfa <kseyeda346@gmail.com>
2025-09-18 02:30:17 -0700

    Qno:2 detailed explain

--- Assignment6/Chap 6 to 9/app.js
+++ Assignment6/Chap 6 to 9/app.js
@@ -20,16 +20,28 @@
 
 // let b= 1;
 
-// let result = --a - --b + ++b + b--;
-
-// console.log(result)
-// document.write(`--a:1;<br/><br/>
-//     --a - --b:1-0=1;<br/><br/>
-//     --a - --b + ++b:1-0+1=2;<br/><br/>
-//     --a - --b + ++b + b--:1-0+1+1=3<br/><br/>
-//     a=1<br/>
-//     b=0<br/>
-//     result=3<hr/>`)
+// // let result = --a - --b + ++b + b--;
+// //               1  - 0 + 1 +1 = 3
+// // console.log(result)
+// // Pre-Decrement of a
+// // --a
+// --a;
+// document.write(`variable of a orignal value is (2) but cause of pre decrement now the value of a is (1) So check the answer is <strong>Ans:${a}</strong><br/><br/>`)
+// // --a - --b
+// // Pre-Decrement of b
+// --b;
+// document.write(`variable of b orignal value is (1) but cause of pre decrement now the value of b is (0) So check the answer is <strong>Ans:${b}</strong><br/><br/>`)
+// // --a - --b + ++b
+// // Pre-Increment
+// ++b;
+// document.write(`Previous value of variable b is (0) but cause of pre increment now the value of b is (1) So check the answer is <strong>Ans:${b}</strong><br/><br/>`)
+// // --a - --b + ++b + b--;
+// // Post-Decrement
+// b--;
+// document.write(`Previous value of variable b is (1) but cause of post decrement now the value of b is (0) but this time value is (1) if we use b is so value is (0) So check the answer is <strong>Ans:${b}</strong><br/><br/>
+//     a:1;<br/>
+//     b:0;<br/>
+//     result:3`)
 
 
 // QUESTION NO#3
@@ -93,61 +105,62 @@
 
 // QUESTION NO#6:a
 
-// let subject1=prompt("Enter your first subject ")
+let subject1=prompt("Enter your first subject ")
 
-// let subject2=prompt("Enter your first subject ")
+let subject2=prompt("Enter your first subject ")
 
-// let subject3=prompt("Enter your first subject ")
-
-
-// // QUESTION NO#6:b
-// let totalMarksEach = 100;
-// let totalMarks = 300;
+let subject3=prompt("Enter your first subject ")
 
 
-// // QUESTION NO#6:c
-// let obtainedMarksSub1 = +prompt("Enter your first subject obtain marks")
+// QUESTION NO#6:b
+let totalMarksEach = 100;
+let totalMarks = 300;
 
 
-// // QUESTION NO#6:d
-// let obtainedMarksSub2 = +prompt("Enter your second subject obtain marks")
+// QUESTION NO#6:c
+let obtainedMarksSub1 = +prompt("Enter your first subject obtain marks")
 
-// let obtainedMarksSub3 = +prompt("Enter your third subject obtain marks")
 
-// let totalObtainedMarks = obtainedMarksSub1 + obtainedMarksSub2 + obtainedMarksSub3
+// QUESTION NO#6:d
+let obtainedMarksSub2 = +prompt("Enter your second subject obtain marks")
 
-// // QUESTION NO#6:e
-// document.write(`<table><tr>
-//     <th>Subject</th>
-//      <th>Total Marks</th>
-//       <th>Obtained Marks</th>
-//        <th>Percentage%</th>
-//     </tr>
-//     <tr>
-//     <td>English</td>
-//      <td>${totalMarksEach}</td>
-//       <td>${obtainedMarksSub1}</td>
-//       <td>${(obtainedMarksSub1/totalMarksEach)*100}%</td>
-//     </tr>
-//       <tr>
-//     <td>Math</td>
-//      <td>${totalMarksEach}</td>
-//       <td>${obtainedMarksSub2}</td>
-//       <td>${(obtainedMarksSub2/totalMarksEach)*100}%</td>
+let obtainedMarksSub3 = +prompt("Enter your third subject obtain marks")
+
+let totalObtainedMarks = obtainedMarksSub1 + obtainedMarksSub2 + obtainedMarksSub3
+
+// QUESTION NO#6:e
+document.write(`<h1>Marksheet in Table</h1>
+    <table border=1><tr>
+    <th>Subject</th>
+     <th>Total Marks</th>
+      <th>Obtained Marks</th>
+       <th>Percentage%</th>
+    </tr>
+    <tr>
+    <td>${subject1}</td>
+     <td>${totalMarksEach}</td>
+      <td>${obtainedMarksSub1}</td>
+      <td>${(obtainedMarksSub1/totalMarksEach)*100}%</td>
+    </tr>
+      <tr>
+    <td>${subject2}</td>
+     <td>${totalMarksEach}</td>
+      <td>${obtainedMarksSub2}</td>
+      <td>${(obtainedMarksSub2/totalMarksEach)*100}%</td>
      
-//     </tr>
-//       <tr>
-//     <td>Urdu</td>
-//      <td>${totalMarksEach}</td>
-//       <td>${obtainedMarksSub3}</td>
-//       <td>${(obtainedMarksSub3/totalMarksEach)*100}%</td>
+    </tr>
+      <tr>
+    <td>${subject2}</td>
+     <td>${totalMarksEach}</td>
+      <td>${obtainedMarksSub3}</td>
+      <td>${(obtainedMarksSub3/totalMarksEach)*100}%</td>
 
-//     </tr>
-//     <tr>
-//     <th></th>
-//     <th>${totalMarks}</th>
-//     <th>${totalObtainedMarks}</th>
-//     <th>${(totalObtainedMarks/totalMarks)*100}%</th>
-//     </tr>
-//     </table>`)
+    </tr>
+    <tr>
+    <th></th>
+    <th>${totalMarks}</th>
+    <th>${totalObtainedMarks}</th>
+    <th>${(totalObtainedMarks/totalMarks)*100}%</th>
+    </tr>
+    </table>`)
   
