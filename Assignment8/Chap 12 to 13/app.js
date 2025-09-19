680f53c32aa942f12a01cec1f7a8b6a050c75e61
Syeda-Kashfa <kseyeda346@gmail.com>
2025-09-19 04:03:22 -0700

    Assignment has been done

---
+++ Assignment8/Chap 12 to 13/app.js
@@ -0,0 +1,112 @@
+// QUESTION NO#1
+
+// let char = prompt("Enter your character to check is Number, uppercase,Lowercase")
+// let input = char.charCodeAt(0);
+//  if(input>=48 && input<=57){
+//     alert(`(${char}) Given input is Number`)
+//  }
+//  else if (input>=65 && input<=90){
+//     alert(`(${char}) Given input is UpperCase`)
+//  }
+//  else if (input>=97 && input<=122){
+//     alert(`(${char}) Given input is LowerCase`)
+//  }
+//  else{
+//     alert(`(${char}) Given input is not a NUMBERS pr not a ALPHABETS is special character or special keys or functional keys`)
+//  }
+
+// QUESTION NO#2
+
+// let numb1 = +prompt("Enter your first number");
+// let numb2 = +prompt("Enter your second number");
+
+// if (numb1>numb2){
+//     alert(`${numb1}: is greater than ${numb2}`)
+// }
+// else if (numb1<numb2){
+//     alert(`${numb1}: is less than ${numb2}`)
+// }
+// else{
+//     alert(`${numb1} is equal to ${numb2}`)
+// }
+
+// QUESTION NO#3
+
+// let number = +prompt("Enter your number")
+
+// if(number>0){
+//     alert(`Given number ( ${number} ) is "POSITIVE"`)
+// }
+// else if(number<0){
+//     alert(`Given number ( ${number} ) is "NEGATIVE"`)
+// }
+// else{
+//     alert(`Given ( ${number} ) is "ZERO"`)
+// }
+
+// QUESTION NO#4
+// let word = prompt(`Enter your Alphabet/Word`)
+
+// if (word==="a" || word==="e" || word==="i" || word==="o" || word==="u" || word==="A" || word==="E" || word==="I" || word==="O" || word==="U"){
+//     alert(`Your alphabet/word is Correct because (${word}) is a vowel word`)
+// }
+// else{
+//     alert(`Your alphabet/word is Incorrect because is a (${word}) is Constant word`)
+// }
+
+// // QUESTION NO#5:a
+
+// let userPassword ;
+
+// // QUESTION NO#5:b
+
+// userPassword = prompt("Enter your Password");
+// let confirmPassword = prompt("Enter your confirm password")
+
+// // QUESTION NO#5:c:1
+
+// if(userPassword===""){
+//     alert(`Please! Enter your password`)
+// }
+
+// // QUESTION NO#5:c:1
+
+// else if(userPassword===confirmPassword){
+//     alert(`Corect!The password you entered matches the orignal password`)
+// }
+// else{
+//     alert(`Incorect!The password you entered not matches the orignal password`) 
+// }
+
+// QUESTION NO#6
+
+// let greeting ;
+// let hour = +prompt("Enter your hour")
+//  if (hour>13 && hour<18){
+//     greeting="Good Day"
+//     alert(`${greeting}`)
+//  }
+//  else{
+//     greeting="Good Evening"
+//     alert(`${greeting} `)
+//  }
+
+// QUESTION NO#7
+
+// let time = +prompt(`Please enter a time in 24 hours \nClock format like 1900=7pm`);
+
+// if(time>=0.00 && time<12.00){
+//     alert("Good Morning")
+// }
+// else if(time>=12.00 && time<17.00){
+//     alert("Good Afternoon")
+// }
+// else if(time>=17.00 && time<21.00){
+//     alert("Good Evening")
+// }
+// else if(time>=21.00 && time<24.00){
+//     alert("Good Night")
+// }
+// else{
+//     alert("Invalid input")
+// }
