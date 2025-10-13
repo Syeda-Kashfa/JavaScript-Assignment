5a247366a0b492ca16fddc1e9a9d5a81f2cc71c9
Syeda-Kashfa <kseyeda346@gmail.com>
2025-10-13 15:13:18 -0700

    complete assignment

---
+++ Assignment12/Chap 26 to 30/app.js
@@ -0,0 +1,62 @@
+// QUESTION NO#1
+
+// let num =3.45214;
+// document.write(`<h1>Original Number: ${num}<hr/>Round off value: ${Math.round(num)}<hr/>Floor value: ${Math.floor(num)}<hr/>Ceil value: ${Math.ceil(num)}`);
+
+// QUESTION NO#2
+
+// let numb=+prompt("Enter any negative number?");
+// document.write(`<h1>Original Number: ${numb}<hr/>Round Value: ${Math.round(numb)}<hr/>Floor Value: ${Math.floor(numb)}<hr/>Ceil Value: ${Math.ceil(numb)}</h1>`);
+
+// QUESTION NO#3
+
+// let numb1=+prompt("Enter any number");
+
+// document.write(`<h1>Original Value: ${numb1}<hr/>Absolute Value: ${Math.abs(numb1)}</h1>`)
+
+// QUESTION NO#4
+
+// let dice=Math.random()*5+1;
+// let diceRoundOff=Math.round(dice);
+// //console.log(dice);
+// document.write(`<h1>Random value dice: ${diceRoundOff}</h1><hr>`)
+
+// QUESTION NO#5
+
+// let coin=Math.random()*1+1;
+// let coin1= Math.round(coin)
+// //  console.log(coin1);
+// if(coin1===2){
+//     document.write(`<h1>${coin1}<br>Random coin value:Heads</h1>`)
+// }
+// else{
+//     document.write(`<h1>${coin1}<br>Random coin value:Tails</h1>`)
+// };
+
+// QUESTION NO#6
+
+// let randomNum=Math.random()*99+1;
+// let final=Math.round(randomNum);
+// document.write(`<h1>Random number between 1 to 100: ${final}</h1>`);
+
+// QUESTION NO#7
+
+// let userNumb=prompt("Enter your wweight in kilograms");
+// let convertNumb= parseFloat(userNumb);
+
+// //console.log(convertNumb.toFixed(2));
+// document.write(`<h1>The weight of user is: ${convertNumb.toFixed(1)} kilograms`)
+
+
+
+// QUESTION NO#8
+//  let secret=Math.random()*10;
+//  let secret1=Math.ceil(secret);
+// let userNum=+prompt("Enter a number between 1 to 10.");
+// if(userNum===secret1){
+//     alert(`Congratulation you win\nYour number is: ${userNum}`)
+// }
+// else{
+//      alert(`Sorry Try again!\nYour number is: ${userNum}`)
+   
+// }
