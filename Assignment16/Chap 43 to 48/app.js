498bfa6a575ee2457da12d3e51363cf9ef9b0fc1
Syeda-Kashfa <kseyeda346@gmail.com>
2025-11-06 23:54:00 +0500

    done

---
+++ Assignment16/Chap 43 to 48/app.js
@@ -0,0 +1,104 @@
+// QUESTION NO#1
+
+// let clck=()=>{
+//     alert("Hello user's");
+// };
+
+// QUESTION NO#2
+
+// let purchase=document.getElementById("pic");
+
+// purchase.addEventListener("click", function(){
+//     alert("Thanks for purchasing a phone from us");
+// });
+
+// let purchase1=document.getElementById("pic1");
+
+// purchase1.addEventListener("click", function(){
+//     alert("Thanks for purchasing a phone from us")
+//     cl.className="hovr"
+// });
+
+// let purchase2=document.getElementById("pic2");
+
+// purchase2.addEventListener("click", function(){
+//     alert("Thanks for purchasing a phone from us")
+//     cl.className="hovr"
+// });
+
+// let purchase3=document.getElementById("pic3");
+
+// purchase3.addEventListener("click", function(){
+//     alert("Thanks for purchasing a phone from us")
+//     cl.className="hovr"
+// });
+
+// QUESTION NO#3
+
+// let butn=document.getElementById("btn1");
+// butn.addEventListener("click", function(){
+//     butn.className="remove"
+// });
+// let butn1=document.getElementById("btn2");
+// butn1.addEventListener("click", function(){
+//     butn1.className="remove"
+// });
+
+// let butn2=document.getElementById("btn3");
+// butn2.addEventListener("click", function(){
+//     butn2.className="remove"
+// });
+// let butn3=document.getElementById("btn4");
+// butn3.addEventListener("click", function(){
+//     butn3.className="remove"
+// });
+
+
+// QUESTION NO#4
+
+// let imag=document.getElementById("imag")
+
+//console.log(imag.src);
+
+// let imagein=()=>{
+//     //console.log(imag.src);
+
+//         imag.src="file:///C:/Users/kashfa/Desktop/JavaScript-Assignment/Assignment16/Chap%2043%20to%2048/images/maxresdefault.jpg"
+//     }
+    
+//     let imageout=()=>{
+//          //console.log(imag.src);   
+//            imag.src="file:///C:/Users/kashfa/Desktop/JavaScript-Assignment/Assignment16/Chap%2043%20to%2048/images/small.jpg" 
+        
+//     }
+
+
+// // QUESTION NO#5;
+// let value=document.getElementById("count");
+// let add=document.getElementById("add");
+// let less=document.getElementById("less");
+
+
+// add.addEventListener("click",function(){
+//     let addVal=document.getElementById("count").innerHTML;
+
+// addVal=Number(addVal);
+
+// addVal=addVal + 1;
+
+// value.innerHTML=addVal
+
+// })
+
+// less.addEventListener("click", function(){
+// let lessVal=document.getElementById("count").innerHTML;
+
+// lessVal=Number(lessVal);
+
+// lessVal=lessVal - 1;
+// value.innerHTML=lessVal  
+
+// })
+
+
+ 
