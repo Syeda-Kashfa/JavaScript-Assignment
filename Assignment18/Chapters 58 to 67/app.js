a72c1f8218d66dfaf9bde13c1438f9ecb639a06d
Syeda-Kashfa <kseyeda346@gmail.com>
2025-11-11 16:14:23 +0500

    completed

---
+++ Assignment18/Chapters 58 to 67/app.js
@@ -0,0 +1,72 @@
+// QUESTION NO#1-a
+
+// let mainContent=document.getElementById("main-content");
+// console.log(mainContent);
+
+
+// // QUESTION NO#1-b
+
+// let inner=mainContent.children
+// console.log(inner);
+
+// for(let i=0 ;i<mainContent.children.length;i++ ){
+//     console.log(mainContent.children[i]);
+    
+// }
+
+// // QUESTION NO#1-c
+
+// let txt=document.getElementsByClassName("render");
+
+// for(let i=0; i<txt.length;i++){
+//     document.write(`${txt[i].innerHTML}`)
+// }
+
+// QUESTION NO#1-d
+
+// let fName=document.getElementById("fname").value="Alex";
+// let lName=document.getElementById("lname").value="Bank";
+// let email=document.getElementById("email").value="alex1428gmail.com";
+
+
+// // QUESTION NO#1-e
+// let fName1=document.getElementById("fname").value="Alex here";
+// let lName1=document.getElementById("lname").value="Bank here";
+
+// QUESTION NO#2-a
+
+// let formContent=document.getElementById("form-content");
+
+// document.write(`Node type Element if answer is 1 so node type is Element is 3 so node type is Text <strong>${formContent.nodeType}</strong>`);
+
+// QUESTION NO#2-b
+
+// let lastName=document.getElementById("lastname");
+// console.log(lastName.nodeType);// node type Element ans(1)
+// console.log(lastName.firstChild.nodeType);//node type 3 ans(3)
+
+// // QUESTION NO#2-c
+
+// let update=lastName.firstChild.nodeValue;
+
+// update=`Last Name: Khan`
+// console.log(update);
+
+// // QUESTION NO#2-d
+
+// let mainContent1=document.getElementById("main-content");
+// console.log(mainContent1.firstElementChild);
+// console.log(mainContent1.lastElementChild);
+
+// // QUESTION NO#2-e
+// let nextSibling=document.getElementById("lastname");
+// console.log("Last Name",nextSibling);
+// console.log("Next Sibling",nextSibling.nextElementSibling);
+// console.log("Previous Sibling",nextSibling.previousElementSibling);
+
+// // QUESTION NO#2-f
+
+// let email=document.getElementById("email");
+// console.log("Email parent node", email.parentNode);
+// console.log("Email parent nodetype", email.nodeType);
+
